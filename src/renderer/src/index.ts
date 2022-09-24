c4bf122a515ac9c5c4fa8a10437ce4e142f12080
nzdjb <github@2a1b.com>
2022-09-24 09:44:46 +0000

    feat: Improve date output.

--- src/renderer/src/index.ts
+++ src/renderer/src/index.ts
@@ -6,10 +6,14 @@ interface Article {
   title: string;
   author?: string;
   date: Date;
+  dateString?: string;
   content: string;
 }
 const config = readdirSync('../articles').map(file => readFileSync(`../articles/${file}`).toString()).join('\n');
 const articles = toml.parse(config)['articles'] as unknown as Article[]
+articles.forEach(article => {
+  article.dateString = article.date.toISOString().split('T')[0];
+});
 articles.sort((a: Article, b: Article) => a.date.getTime() - b.date.getTime());
 articles.reverse();
 const index = readFileSync('../templates/index.hb.html').toString();

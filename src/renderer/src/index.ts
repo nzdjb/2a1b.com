9c194942123aee8215a4bce77a8afee6c0e77660
nzdjb <github@2a1b.com>
2022-09-24 09:34:28 +0000

    feat: Initial rework to toml based articles.

---
+++ src/renderer/src/index.ts
@@ -0,0 +1,17 @@
+import Handlebars from "handlebars";
+import toml from '@iarna/toml';
+import { readFileSync, readdirSync, writeFileSync } from "fs";
+
+interface Article {
+  title: string;
+  author?: string;
+  date: Date;
+  content: string;
+}
+const config = readdirSync('../articles').map(file => readFileSync(`../articles/${file}`).toString()).join('\n');
+const articles = toml.parse(config)['articles'] as unknown as Article[]
+articles.sort((a: Article, b: Article) => a.date.getTime() - b.date.getTime());
+articles.reverse();
+const index = readFileSync('../templates/index.hb.html').toString();
+const output = Handlebars.compile(index)({ articles });
+writeFileSync('../../dist/index.html', output);
